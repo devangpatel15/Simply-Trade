f9266f62b19ae41dcd4d0e528a79e2760208362d
devangpatel15 <devang150803@gmail.com>
2025-03-11 18:32:51 +0530

    huih

--- backend/models/color.js
+++ backend/models/color.js
@@ -1,16 +1,14 @@
 const mongoose = require("mongoose");
 
 const colorSchema = new mongoose.Schema(
-    {
-        colorName : { type: String, required: true },
-        deviceId : { type: mongoose.Schema.Types.ObjectId, ref: 'Device', required: true }, 
-    },{timestamps: true}
-<<<<<<< HEAD
-)
-
+  {
+    colorName: { type: String, required: true },
+    deviceId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Device",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 module.exports = mongoose.model("Color", colorSchema);
-=======
-
-)
-module.exports = mongoose.model("Color", colorSchema);
->>>>>>> be9e9958578d54aef75ddcd728c65f5524733671
--- backend/models/device.js
+++ backend/models/device.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
 
 const deviceSchema = new mongoose.Schema(
-    {
-        deviceName : { type: String, required: true },
-        modelId : { type: mongoose.Schema.Types.ObjectId, ref: 'Model', required: true },
-    },{timestamps: true}
-)
+  {
+    deviceName: { type: String, required: true },
+    modelId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Model",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-<<<<<<< HEAD
 module.exports = mongoose.model("Device", deviceSchema);
-=======
-module.exports = mongoose.model("Device", deviceSchema);
->>>>>>> be9e9958578d54aef75ddcd728c65f5524733671
