9dd623fcc0bf9eb3965a176829180f05ea6478b1
khushihadvani <hadvanikhushi@gmail.com>
2025-03-17 15:26:24 +0530

    create branch with new org

--- backend/routes/organizationBranch.js
+++ backend/routes/organizationBranch.js
@@ -1,10 +1,11 @@
 const express =require('express')
-const {  findAllOrganizationBranch, deleteOrganizationBranch, updateOrganizationBranch, findOneOrganizationBranch, createOrganizationBranch} = require('../controllers/OrganizationBranch')
+const {  findAllOrganizationBranch, deleteOrganizationBranch, updateOrganizationBranch, findOneOrganizationBranch, createOrganizationBranch, findUserOrganizationBranch} = require('../controllers/OrganizationBranch')
 const { validateGetOneOrganizationBranchData, validateCreateOrganizationBranchData, validateUpdateOrganizationBranchData, validateDeleteOrganizationBranchData } = require('../middleware/OrganizationBranch')
 const { AuthUser } = require('../middleware/user')
 const organizationBranchRoute=express.Router()
 
 organizationBranchRoute.get('/findAllOrganizationBranch',AuthUser,findAllOrganizationBranch)
+organizationBranchRoute.get('/findAllUserOrganizationBranch',AuthUser,findUserOrganizationBranch)
 organizationBranchRoute.get('/findOneOrganizationBranch/:id',AuthUser,validateGetOneOrganizationBranchData,findOneOrganizationBranch)
 organizationBranchRoute.post('/createOrganizationBranch',AuthUser,validateCreateOrganizationBranchData,createOrganizationBranch)
 organizationBranchRoute.put('/updateOrganizationBranch/:id',AuthUser,validateUpdateOrganizationBranchData,updateOrganizationBranch)
