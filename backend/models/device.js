6634a93bc03d463254297719c4f0047243cf4e42
Vidhi019 <vidhisheth742@gmail.com>
2025-03-19 15:45:19 +0530

    add id in models

--- backend/models/device.js
+++ backend/models/device.js
@@ -3,11 +3,23 @@ const mongoose = require("mongoose");
 const deviceSchema = new mongoose.Schema(
   {
     deviceName: { type: String, required: true },
+    categoryId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category",
+      },
     modelId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Model",
       required: true,
     },
+     organization : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Organization",
+      },
+      branchName : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "OrganizationBranch",
+      },
     isDeleted: { type: Boolean, default: false },
   },
   { timestamps: true }
