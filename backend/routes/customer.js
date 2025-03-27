f1a10bb9191ed3c7bd25fe040e6bba1e2025ce80
khushihadvani <hadvanikhushi@gmail.com>
2025-03-27 16:16:16 +0530

    add selectCustomer

--- backend/routes/customer.js
+++ backend/routes/customer.js
@@ -1,11 +1,18 @@
 const express = require('express')
-const { getAllCustomer, getCustomer, createCustomer, updateCustomer, deleteCustomer, softDeleteCustomer, searchCustomer } = require('../controllers/customer')
+const { getAllCustomer, getCustomer, createCustomer, updateCustomer, deleteCustomer, softDeleteCustomer, searchCustomer, selectCustomer } = require('../controllers/customer')
 const { customerIdValidation, createCustomerValidation, updateCustomerValidation } = require('../middleware/customer')
+const { AuthUser } = require('../middleware/user')
 
 const cusRouter = express.Router()
 
 cusRouter.get("/allCustomer",getAllCustomer)
 cusRouter.get("/searchCustomer",searchCustomer)
+
+cusRouter.get(
+  "/selectCustomer/:id",
+  AuthUser,
+  selectCustomer
+);
 cusRouter.get("/customer/:id",customerIdValidation,getCustomer)
 cusRouter.post("/createCustomer",createCustomerValidation,createCustomer)
 cusRouter.put("/updateCustomer/:id",updateCustomerValidation,updateCustomer)
