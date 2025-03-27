010c0466ddc9863eaf385cc8b2fa2b1334ff597c
harvi320 <patelharvi969@gmail.com>
2025-03-27 14:03:12 +0530

    stock api

--- frontend/src/apis/StockApi.js
+++ frontend/src/apis/StockApi.js
@@ -13,4 +13,61 @@ const createStock = async (formData) => {
   }
 };
 
-export { createStock };
+const updateStock = async (formData, id) => {
+  try {
+    await axios.put(`http://localhost:4000/api/updateStock/${id}`, formData, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+  } catch (error) {
+    console.log(error, "Update Stock error");
+  }
+};
+
+const getOneStock = async (id) => {
+  try {
+    const respone = await axios.get(`http://localhost:4000/api/stock/${id}`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    return respone;
+  } catch (error) {
+    console.log(error, "get Stock error");
+  }
+};
+
+const getAllStocks = async () => {
+  try {
+    const response = await axios.get("http://localhost:4000/api/allStock", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    return response;
+  } catch (error) {
+    console.log(error, "get All Stock error");
+  }
+};
+const deleteStock = async (id) => {
+  try {
+    await axios.put(
+      `http://localhost:4000/api/deleteStock/${id}`,
+      {},
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
+  } catch (error) {
+    console.log(error, "get All Stock error");
+  }
+};
+
+export { createStock, updateStock, getOneStock, getAllStocks, deleteStock };
