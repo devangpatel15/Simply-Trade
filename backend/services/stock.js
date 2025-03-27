74d5d3cdd9de6d41b4ec8700d8868089d0d8f41f
devangpatel15 <devang150803@gmail.com>
2025-03-27 14:34:01 +0530

    backend

--- backend/services/stock.js
+++ backend/services/stock.js
@@ -1,25 +1,32 @@
-const Stock = require('../models/stock');
+const Stock = require("../models/stock");
 
 exports.getAllStockService = async () => {
-  return await Stock.find({ isDeleted: false }).lean();
+  return await Stock.find({ isDeleted: false })
+    .populate(
+      "organization branch categoryName modelName deviceName capacityName color"
+    )
+    .lean();
 };
 
 exports.getStockService = async (stockId) => {
-  return await Stock.findById(stockId).populate("organization branch categoryName modelName deviceName capacityName color").lean();
+  return await Stock.findById(stockId)
+    .populate(
+      "organization branch categoryName modelName deviceName capacityName color"
+    )
+    .lean();
 };
 
 exports.createStockService = async (newStock) => {
-  return await Stock.create(newStock)
+  return await Stock.create(newStock);
 };
 
 exports.updateStockService = async (stockId, stock) => {
-  return await Stock.findByIdAndUpdate(stockId, stock,{new:true}).lean();
+  return await Stock.findByIdAndUpdate(stockId, stock, { new: true }).lean();
 };
 
 exports.softDeleteStockService = async (stockId) => {
   return await Stock.findByIdAndUpdate(stockId, { isDeleted: true });
 };
-
 
 exports.deleteStockService = async (stockId) => {
   return await Stock.findByIdAndDelete(stockId);
